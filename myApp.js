725aa088c2788cfdd17fe19b2d8c150d4594564b
elaintran <elain.ttran@gmail.com>
2019-02-09 22:55:31 -0600

    express testing

--- myApp.js
+++ myApp.js
@@ -22,13 +22,22 @@ app.get("/", function(req, res) {
 });
 
 /** 4) Serve static assets  */
-
+app.use(express.static(__dirname + "/public"));
 
 /** 5) serve JSON on a specific route */
-
+app.get("/json", function(req, res) {
+    res.json({"message": "Hello json"});
+});
 
 /** 6) Use the .env file to configure the app */
- 
+var MESSAGE_STYLE = "Hello json".toUpperCase();
+
+if (process.env.VAR_NAME === "allCaps") {
+	MESSAGE_STYLE = "Hello json".toUpperCase();
+	}
+else {
+	MESSAGE_STYLE = "Hello json";
+}
  
 /** 7) Root-level Middleware - A logger */
 //  place it before all the routes !
